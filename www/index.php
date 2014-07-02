c0880c7611ef53daedfce2ae3872d0b394105637
gabrielhoffman <gabrielhoffman@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-07-02 15:47:19 +0000

    v 0.1.1
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/lrgpr@123 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -69,7 +69,7 @@ The package also provides simple interfaces for standard (i.e. fixed effects) li
 
 <li> <a href="./docs/INSTALL.pdf"><strong>Installation instructions</strong></a>: how to install dependencies (<i>essential</i>)</li>
 
-<li> <a href="./docs/lrgpr_0.1.0.tar.gz"><strong>Download package</strong></a>: v0.1.0 </li>
+<li> <a href="./docs/lrgpr_0.1.1.tar.gz"><strong>Download package</strong></a>: v0.1.1 </li>
 
 <br>
 
@@ -86,7 +86,7 @@ The package also provides simple interfaces for standard (i.e. fixed effects) li
 <br>
 <li> <strong>Citation</strong>:</li>
 
- Hoffman, G.E., Mezey, J.G., Schadt, E.E. (2014) lrgpr: Interactive linear mixed model analysis of genome-wide association studies with composite hypothesis testing and regression diagnostics in R. In preparation. </p>
+ Hoffman, G.E., Mezey, J.G., Schadt, E.E. (2014) lrgpr: Interactive linear mixed model analysis of genome-wide association studies with composite hypothesis testing and regression diagnostics in R. In press. </p>
 
 <li> <strong>Related work</strong>:</li>
 
@@ -103,7 +103,7 @@ The package also provides simple interfaces for standard (i.e. fixed effects) li
 <hr />
 <font size="1">
 <div id="textbox">
-  <p class="alignleft">Gabriel E. Hoffman.  Updated June 10, 2014</p>
+  <p class="alignleft">Gabriel E. Hoffman.  Updated July 2, 2014</p>
   <!--<p class="alignright">
   	<?
 	echo "UPV: ";
